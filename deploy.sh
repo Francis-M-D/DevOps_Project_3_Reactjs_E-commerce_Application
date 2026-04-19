d5afee9d20deea83224fcfa8c1c9a99d5a5f22a6
Francis-D-M <francis2882001@gmail.com>
2026-04-19 05:31:12 +0000

    deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-docker compose down --remove-orphans      # Removes containers for services not defined in the current YAML
+docker-compose down --remove-orphans      # Removes containers for services not defined in the current YAML
 docker rm -f react-app 2>/dev/null || true # Forcefully remove the specific name if it still exists
-docker compose pull
-docker compose up -d --force-recreate     # Ensures containers are replaced even if no config changed
-
+docker-compose pull
+docker-compose up -d --force-recreate     # Ensures containers are replaced even if no config changed
