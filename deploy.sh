94800d88fc9fbc2d0411ac4a38df887eb125e58c
Francis-D-M <francis2882001@gmail.com>
2026-04-19 05:20:53 +0000

    Testing

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
+echo "docker-compose down"
 docker-compose down
+echo "docker-compose pull"
 docker-compose pull
+echo "docker-compose build --no-cache"
 docker-compose build --no-cache
+echo "docker-compose up -d"
 docker-compose up -d
